67bf538e9bf63b953d4df975d447487a3d81ea3e
Carlos Gavidia-Calderon <cgavidia-calderon@turing.ac.uk>
2025-05-30 17:05:08 +0100

    Interstitial docs

--- interstitial.py
+++ interstitial.py
@@ -5,8 +5,11 @@ from bitmapfont import BitmapFont
 
 class InterstitialState(GameState):
     def __init__(
-        self, game: Game, message: str, wait_time_ms, next_state: GameState | None
+        self, game: Game, message: str, wait_time_ms: int, next_state: GameState | None
     ) -> None:
+        """
+        :param game: Game instance.
+        """
         super().__init__(game)
 
         self.next_state: GameState | None = next_state
@@ -15,6 +18,11 @@ class InterstitialState(GameState):
         self.wait_timer: int = wait_time_ms
 
     def update(self, game_time: int):
+        """
+        Waits until the timer runs down. When timer reaches zero, game moves to the next state.
+
+        :param game_time: Game time.
+        """
         self.wait_timer -= game_time
         if self.wait_timer < 0:
             self.game.change_state(self.next_state)
