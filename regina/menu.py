c5640d98219251ddfd250f078bed8a53a352e8c7
Dr Griffith Rees <griff.rees@gmail.com>
2025-06-09 13:44:50 +0100

    feat: add `regina/menu.py`

---
+++ regina/menu.py
@@ -0,0 +1,87 @@
+from framework import Game, GameState
+from bitmapfont import BitmapFont
+import pygame
+from pygame.key import ScancodeWrapper
+from pygame.locals import K_UP, K_DOWN, K_SPACE
+from pygame.surface import Surface
+
+from dataclasses import dataclass
+from typing import Sequence, Literal
+from os import PathLike
+
+MENU_ITEMS: Literal = ("Start", "Resume", "Quit")
+
+MENU_BACKGROUND: PathLike = "assets/img/2010-08-03_British_Library_exterior_02.jpg"
+
+@dataclass
+class MainMenuState(GameState):
+
+    play_game_state: GameState | None = None
+    font: BitmapFont = BitmapFont("assets/img/2010-08-03_British_Library_exterior_02.jpg", 12, 12)
+    index: int = 0
+    input_tick: int = 0
+    menu_items: Sequence[str] = MENU_ITEMS
+
+    # def __init__(self, game: Game) -> None:
+    #     super().__init__(game)
+    #
+    #     self.play_game_state: GameState | None = None
+    #     self.font: BitmapFont = BitmapFont("img/fasttracker2-style_12x12.png", 12, 12)
+    #     self.index: int = 0
+    #     self.input_tick: int = 0
+    #     self.menu_items: list[str] = ["Start game", "Quit"]
+
+    def __post_init__(self, game: Game) -> None:
+        super().__init__(game)
+        #
+        # self.play_game_state: GameState | None = None
+        # self.font: BitmapFont = BitmapFont("img/fasttracker2-style_12x12.png", 12, 12)
+        # self.index: int = 0
+        # self.input_tick: int = 0
+        # self.menu_items: list[str] = ["Start game", "Quit"]
+
+    def set_play_state(self, state) -> None:
+        self.play_game_state = state
+
+    def update(self, game_time: int) -> None:
+        keys: ScancodeWrapper = pygame.key.get_pressed()
+
+        if keys[K_UP] or keys[K_DOWN] and self.input_tick == 0:
+            self.input_tick = 250
+
+            if keys[K_UP]:
+                self.index -= 1
+                if self.index < 0:
+                    self.index = len(self.menu_items) - 1
+
+            elif keys[K_DOWN]:
+                self.index += 1
+                if self.index == len(self.menu_items):
+                    self.index = 0
+        elif self.input_tick > 0:
+            self.input_tick -= game_time
+
+        if self.input_tick < 0:
+            self.input_tick = 0
+
+        if keys[K_SPACE]:
+            if self.index == 1:
+                self.game.change_state(None)
+            elif self.index == 0:
+                self.game.change_state(self.play_game_state)
+
+    def draw(self, surface: Surface) -> None:
+        self.font.centre(surface, "Invaders! From Space", 48)
+        count: int = 0
+        y = surface.get_rect().height - len(self.menu_items) * 160
+
+        for item in self.menu_items:
+            item_text: str = " "
+
+            if count == self.index:
+                item_text = "> "
+            item_text += item
+            self.font.draw(surface, item_text, 25, y)
+
+            y += 24
+            count += 1
