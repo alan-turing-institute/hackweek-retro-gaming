96bb83b9b9ab548104d8920438d537f365a475f8
Dr Griffith Rees <griff.rees@gmail.com>
2025-06-11 13:52:48 +0100

    fix: menu background image

--- reg-game/config.py
+++ reg-game/config.py
@@ -4,9 +4,9 @@ from pathlib import Path
 
 MENU_TITLE: str = "Regina Quest!"
 MENU_FONT_IMG: Path = Path("img/fasttracker2-style_12x12.png")
-MENU_BACKGROUND_PATH: Path = Path("img/2010-08-03_British_Library_exterior_02.jpg")
-MENU_BACKGROUND_POSITION: tuple[int, int] = (-180, -180)
-MENU_BACKGROUND_SCALE_FACTOR: float = 0.3
+MENU_BACKGROUND_PATH: Path = Path("img/british-library-14880.jpg")
+MENU_BACKGROUND_POSITION: tuple[int, int] = (0, 0)
+MENU_BACKGROUND_SCALE_FACTOR: float = 1.0
 
 MENU_ITEMS: tuple[str, ...] = ("Start", "Resume", "Quit")
 
