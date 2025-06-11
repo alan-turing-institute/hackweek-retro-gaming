da0fd80161614b2f5a5d1a7b003ec877e4ebd9e0
Matt Craddock <5796417+craddm@users.noreply.github.com>
2025-06-11 12:00:54 +0100

    Use newer main theme, add extra sounds

--- reg-game/sounds.py
+++ reg-game/sounds.py
@@ -6,7 +6,7 @@ class MusicPlayer:
         """
         Initializes the MusicPlayer instance.
         """
-        self.game_theme = mixer.music.load("sound/reggame.mp3")
+        self.game_theme = mixer.music.load("sound/reggame-v2.mp3")
         self.is_playing = False
         self.volume = 0.3
 
@@ -42,11 +42,33 @@ class SoundEffectPlayer:
         """
         Initializes the SoundEffectPlayer instance.
         """
+        self.hacker_alert = mixer.Sound("sound/hackeralert.ogg")
+        self.hacker_alert.set_volume(0.5)
         self.sandbox_sound = mixer.Sound("sound/playershoot.wav")
         self.sandbox_sound.set_volume(0.5)
+        self.hacking_sound = mixer.Sound("sound/hackingnoise-shorter.wav")
+        self.hacking_sound.set_volume(0.05)
 
     def play_sandbox_sound(self):
         """
         Plays the sandbox sound effect.
         """
         self.sandbox_sound.play()
+
+    def play_hacker_alert(self):
+        """
+        Plays the hacker alert sound effect.
+        """
+        self.hacker_alert.play()
+
+    def play_hacking_sound(self):
+        """
+        Plays the hacking sound effect.
+        """
+        self.hacking_sound.play(loops = -1)
+
+    def stop_hacking_sound(self):
+        """
+        Plays the hacking sound effect.
+        """
+        self.hacking_sound.stop()
