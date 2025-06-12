764f7790b495034689a962288a60bc22f3f24ae0
Matt Craddock <5796417+craddm@users.noreply.github.com>
2025-06-13 00:23:30 +0100

    Make GAME OVER longer so you can listen to the music longer :D

--- reg-game/main.py
+++ reg-game/main.py
@@ -4,12 +4,11 @@ from interstitial import InterstitialState
 from menu import MainMenuState
 from reg_game import PlayGameState
 
-
 reg_game: Game = Game("The REG Game", SCREEN_WIDTH, SCREEN_HEIGHT)
 
 main_menu_state: MainMenuState = MainMenuState(reg_game)
 game_over_state: InterstitialState = InterstitialState(
-    reg_game, "G A M E  O V E R !", 5000, main_menu_state
+    reg_game, "G A M E  O V E R !", 12000, main_menu_state
 )
 
 play_game_state: PlayGameState = PlayGameState(reg_game, game_over_state)
